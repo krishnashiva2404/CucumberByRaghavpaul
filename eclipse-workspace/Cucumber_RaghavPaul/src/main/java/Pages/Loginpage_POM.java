6d04485edcfe1c426bab2f26c77c979d066da228
pshivakrishna753 <pshivakrishna753@gmail.com>
2024-07-26 07:15:48 +0530

    POM login file commit by shiva

--- eclipse-workspace/Cucumber_RaghavPaul/src/main/java/Pages/Loginpage_POM.java
+++ eclipse-workspace/Cucumber_RaghavPaul/src/main/java/Pages/Loginpage_POM.java
@@ -1,5 +1,33 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Loginpage_POM {
+	
+	WebDriver driver;
+	
+	By txt_username= By.name("username");
+	
+	By txt_password= By.name("password");
+	
+	By btn_login= By.xpath("//button[@type='submit']");
+	
+	public Loginpage_POM(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	public void setusername(String username) {
+		
+		driver.findElement(txt_username).sendKeys(username);
+	}
+	
+	public void setpassword(String password) {
+		driver.findElement(txt_password).sendKeys(password);
+	}
+	
+	public void clicklogin() {
+		driver.findElement(btn_login).click();
+	}
 
 }
